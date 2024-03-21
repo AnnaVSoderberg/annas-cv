94ed2a32645d1aa941910a8f3fd759602299d5b7
Anna Söderberg <soderberganna@outlook.com>
2024-03-21 12:06:44 +0100

    added react-icons and added it to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,13 +1,20 @@
 
 import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Footer(){
     return(
 <footer>
     <a href="https://www.linkedin.com/in/anna-s%C3%B6derberg-a486897b/" target="_blank" title='LinkedIn-profil'>
-    <i className="fa-brands fa-linkedin"></i>
+    <FaLinkedin className="fa-brands fa-linkedin" />
     </a>
     <p className="footernumber">Telefon: 0703 88 37 80</p>
 </footer>
     );
 }
+
+
+
+
+
+//TESTET B
